887755d58e5fab1d9c7a8b1b50584bc500fcaebc
Diego050109 <lemadiego597@gmail.com>
2026-01-19 15:25:37 -0500

    feat(history-service): add health controller

---
+++ services/history-service/src/health/health.controller.ts
@@ -0,0 +1,13 @@
+﻿import { Controller, Get } from '@nestjs/common';
+
+@Controller('health')
+export class HealthController {
+  @Get()
+  health() {
+    return {
+      status: 'ok',
+      service: 'history-service',
+      timestamp: new Date().toISOString(),
+    };
+  }
+}
